c91e8623c7dd57752a88d37e2683e7fe84093afb
Gustavo P. Santana <gustavopsantana4@gmail.com>
2025-06-07 11:15:35 -0300

    Acesso Rápido

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import Header from './components/header';
 import MainBanner from './components/mainBanner';
 import FastLinks from './components/fastLinks';
 
-import './style/base.css';
-
 function App() {
   return (
     <React.Fragment>
--- src/components/mainBanner.tsx
+++ src/components/mainBanner.tsx
@@ -5,7 +5,7 @@ import '../style/mainBanner.css';
 function MainBanner() {
   return (
     <div className="main-banner">
-      <img src={bannerImage} className="banner-image" alt="Banner Image"/>
+      <img src={bannerImage} className="banner-image" alt="Banner"/>
     </div>
   );
 }
