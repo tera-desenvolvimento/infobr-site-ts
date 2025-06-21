ab857295e63b002696a61055b255364326daa589
Suporte Tera <suporte@tera.tec.br>
2025-06-20 22:37:43 -0300

    Ajustes

--- src/components/customers.tsx
+++ src/components/customers.tsx
@@ -1,89 +1,63 @@
-import Slider from "react-slick";
-
-import logoBlueImg from "../img/customer-logo.svg";
+import customer01 from "../img/customers/cus01.svg";
+import customer02 from "../img/customers/cus02.svg";
+import customer03 from "../img/customers/cus03.svg";
+import customer04 from "../img/customers/cus04.svg";
+import customer05 from "../img/customers/cus05.svg";
+import customer06 from "../img/customers/cus06.svg";
+import customer07 from "../img/customers/cus07.svg";
+import customer08 from "../img/customers/cus08.svg";
+import customer09 from "../img/customers/cus09.svg";
+import customer10 from "../img/customers/cus10.svg";
+import customer11 from "../img/customers/cus11.svg";
+import customer12 from "../img/customers/cus12.svg";
+import customer13 from "../img/customers/cus13.svg";
+import customer14 from "../img/customers/cus14.svg";
+import customer15 from "../img/customers/cus15.svg";
+import customer16 from "../img/customers/cus16.svg";
 
 import "../style/customers.css"
 
 function Customers() {
-    const sliderSettings = {
-        dots: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 1000,
-        autoplaySpeed: 3000,
-        cssEase: "linear",
-        responsive: [
-        {
-            breakpoint: 2700,
-            settings: {
-            slidesToShow: 5,
-            slidesToScroll: 5,
-            infinite: true,
-            dots: false
-            }
-        },
-        {
-            breakpoint: 1500,
-            settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false
-            }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            initialSlide: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-            }
-        }
-        ]
-    };
+    
 
     return (
-        <div className="customers-container">
+        <div className="customers-container slider">
             <b>Alguns de nossos clientes</b>
 
-            <Slider {...sliderSettings} className='customers-slider'>
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-                <img src={logoBlueImg} className="logo-customer" />
-            </Slider>
+            <div className='customers-slider slide-track'>
+                <img src={customer01} className="logo-customer slide" />
+                <img src={customer02} className="logo-customer slide" />
+                <img src={customer03} className="logo-customer slide" />
+                <img src={customer04} className="logo-customer slide" />
+                <img src={customer05} className="logo-customer slide" />
+                <img src={customer06} className="logo-customer slide" />
+                <img src={customer07} className="logo-customer slide" />
+                <img src={customer08} className="logo-customer slide" />
+                <img src={customer09} className="logo-customer slide" />
+                <img src={customer10} className="logo-customer slide" />
+                <img src={customer11} className="logo-customer slide" />
+                <img src={customer12} className="logo-customer slide" />
+                <img src={customer13} className="logo-customer slide" />
+                <img src={customer14} className="logo-customer slide" />
+                <img src={customer15} className="logo-customer slide" />
+                <img src={customer16} className="logo-customer slide" />
+                <img src={customer01} className="logo-customer slide" />
+                <img src={customer02} className="logo-customer slide" />
+                <img src={customer03} className="logo-customer slide" />
+                <img src={customer04} className="logo-customer slide" />
+                <img src={customer05} className="logo-customer slide" />
+                <img src={customer06} className="logo-customer slide" />
+                <img src={customer07} className="logo-customer slide" />
+                <img src={customer08} className="logo-customer slide" />
+                <img src={customer09} className="logo-customer slide" />
+                <img src={customer10} className="logo-customer slide" />
+                <img src={customer11} className="logo-customer slide" />
+                <img src={customer12} className="logo-customer slide" />
+                <img src={customer13} className="logo-customer slide" />
+                <img src={customer14} className="logo-customer slide" />
+                <img src={customer15} className="logo-customer slide" />
+                <img src={customer16} className="logo-customer slide" />
+            </div>
         </div>
     )
 }
--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -19,7 +19,7 @@ function Testimonial() {
             breakpoint: 2700,
             settings: {
             slidesToShow: 5,
-            slidesToScroll: 5,
+            slidesToScroll: 1,
             infinite: true,
             dots: false
             }
@@ -28,7 +28,7 @@ function Testimonial() {
             breakpoint: 1500,
             settings: {
             slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToScroll: 1,
             infinite: true,
             dots: false
             }
@@ -37,7 +37,7 @@ function Testimonial() {
             breakpoint: 1024,
             settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
             dots: false
             }
@@ -46,7 +46,7 @@ function Testimonial() {
             breakpoint: 600,
             settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToScroll: 1,
             initialSlide: 2
             }
         },
